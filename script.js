3927c64f788cc9f735fbaa98de86f3bb0317ba75
Fernando <fernandopavonifc@gmail.com>
2026-09-21 16:57:00 -0300

    atualizaçao do projeto

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function irParaProximaTela() {
+    // Aqui você pode colocar validações se quiser
+    console.log("a funçao foi chamada")
+    // Comando que muda a página:
+    window.location.href = "pg2.html";
+}
